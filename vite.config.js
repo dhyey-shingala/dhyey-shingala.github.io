ce83351ec142b8fc24558ec51069503732c49c4c
Dhyey Shingala <dhyeyshingala@protonmail.com>
2024-04-09 23:15:13 -0500

    changed .js file

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from "vite";
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    base: "./dhyey-shingala",
-    build: {
-        minify: "terser",
-    },
+  plugins: [react()],
+  base: '/my-vite-app/'
 })
