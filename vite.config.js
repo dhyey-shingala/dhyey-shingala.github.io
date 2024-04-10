05645e06fda152f2e5b87cf1892fe711e24561f1
Dhyey <dhyeyshingala@protonmail.com>
2024-04-09 23:44:38 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [],
-  base: '/my-vite-app/'
-})
+  base: "./",
+  build: {
+    minify: "terser",
+  },
+});
