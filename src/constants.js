fcafce13ac22498b09122d69c8a83a839c4a753c
Dhyey Shingala <dhyeyshingala@protonmail.com>
2024-04-10 03:43:00 -0500

    new portfolio

---
+++ src/constants.js
@@ -0,0 +1,20 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `This is my PC. I work mostly in ASP.NET and Linux these days. I use Arch by the way.
+      I've made a bunch of projects in ASP.NET. I also like TypeScript and Python. Anyway regardless of the language, I just like programming.
+      Here is my <a href="https://github.com/dhyey-shingala" target="_blank">Github</a>!`,
+    "cs-degree": `This is my degree, I have a master's in computer science. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+    "sofa-table": `That's my sofa. I like to relax here and watch YouTube. I also watch a lot of tutorials on Udemy to improve my skills and keep up with the latest trends.`,
+    tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+     <a href="https://www.youtube.com/@NetworkChuck" target="_blank">NetworkChuck</a>, <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">ThePrimeagen</a>,
+    <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@rwxrob" target="_blank">rwxrob</a>!`,
+    bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+    resume: `This is my desk and on it is my resume. <a href="https://drive.google.com/file/d/1qkyiziMJKT1C2FIXIHTuktTMBrRlhbi4/view?usp=sharing" target="_blank">Check it out?</a>
+    Contact me at shingaladhyey@gmail.com if you have any interesting job opportunities!`,
+    projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+    Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about my projects 
+    on my github page <a href="https://github.com/dhyey-shingala?tab=repositories">here.</a>`,
+    library: `There are a lot of books on my shelves. I have a ton of novels and a bunch of programming books.`,
+    exit: `If you want to exit Dhyey's portfolio, just close the tab.`,
+  };
