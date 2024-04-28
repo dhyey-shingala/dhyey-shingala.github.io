a7608b1e9bd7f44777e4ae8bba525029a61aa246
Dhyey <dhyeyshingala@protonmail.com>
2024-04-28 15:48:29 -0700

    Updated resume link

--- src/constants.js
+++ src/constants.js
@@ -10,7 +10,7 @@ export const dialogueData = {
      <a href="https://www.youtube.com/@NetworkChuck" target="_blank">NetworkChuck</a>, <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">ThePrimeagen</a>,
     <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@rwxrob" target="_blank">rwxrob</a>!`,
     bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
-    resume: `This is my desk and on it is my resume. <a href="https://drive.google.com/file/d/1qkyiziMJKT1C2FIXIHTuktTMBrRlhbi4/view?usp=sharing" target="_blank">Check it out?</a>
+    resume: `This is my desk and on it is my resume. <a href="https://drive.google.com/file/d/1fZFgBZFjK0yWBao7rgeLXe2joUj8O-5e/view?usp=sharing" target="_blank">Check it out?</a>
     Contact me at shingaladhyey@gmail.com if you have any interesting job opportunities!`,
     projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
     Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about my projects 
